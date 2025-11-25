dee0b1151ecff6fedddfc389ac5900e7df01af9a
shibowang1 <2273959487@qq.com>
2025-11-25 15:36:51 +0800

    111

--- app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
@@ -7,6 +7,9 @@ import com.example.myapplication.ui.theme.DoubaoTheme
 
 import com.example.myapplication.ui.pages.DoubaoScreen
 
+//输入 “code” 查看代码输出的渲染效果
+//输入 “picture” 查看图片的输出效果
+//输出其他任意的字符 查看一般输出文字的效果
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
